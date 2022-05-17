ee584242ca305762f3a6870a7c0288cf93d5845f
Lucas Couto <loc.unb@gmail.com>
2022-05-16 23:17:11 -0300

    Adiciona requisição a cada 10min

--- src/app/weather-container/weather-card/weather-card.component.ts
+++ src/app/weather-container/weather-card/weather-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { interval, mergeMap, timer } from 'rxjs';
 import { City } from 'src/app/models/city';
 import { WeatherInfo } from 'src/app/models/weather-info';
 import { WeatherService } from 'src/app/services/weather.service';
@@ -11,13 +12,17 @@ import { WeatherService } from 'src/app/services/weather.service';
 export class WeatherCardComponent implements OnInit {
   @Input() city!: City;
   currentWeather!: WeatherInfo;
+  updateHour = new Date();
 
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
-    this.weatherService
-      .getCurrentWeather(this.city.name)
-      .subscribe((weather) => (this.currentWeather = weather));
+    timer(0, 10 * 60 * 1000)
+      .pipe(mergeMap(() => this.loadData()))
+      .subscribe((weather) => {
+        this.currentWeather = weather;
+        this.updateHour = new Date();
+      });
   }
 
   getTemperatureColor(): Object {
@@ -27,5 +32,9 @@ export class WeatherCardComponent implements OnInit {
         this.currentWeather.temp > 5 && this.currentWeather.temp <= 25,
       'high-temperature': this.currentWeather.temp > 25,
     };
+  }
+
+  private loadData() {
+    return this.weatherService.getCurrentWeather(this.city.name);
   }
 }
